bcdd99fe751d6315156cf9a73fa7be7f154d73b0
an.han <an.han@qunar.com>
2015-08-13 23:05:49 +0800

    add done

--- scripts/format.js
+++ scripts/format.js
@@ -1,8 +1,6 @@
 /**
  * Created by an.han on 15/8/8.
  */
-var fs = require('fs');
-var path = require('path');
 
 console.log('INFO Format data start');
 
@@ -12,15 +10,14 @@ hexo.on('generateBefore', function () {
     hexo.locals.get('posts').each(function (post) {
         var year = post.date.year();
         var index = 0;
-        var arr = yearMap[year];
-        if (!arr) {
-            arr = yearMap[year] = [post];
+        if (!yearMap[year]) {
+            yearMap[year]  = [post];
         }
         else {
-            while( arr[index] && (post.date.valueOf() < arr[index].date.valueOf()) ){
+            while( yearMap[year][index] && (post.date.valueOf() < yearMap[year][index].date.valueOf()) ){
                 index++;
             }
-            arr.splice(index, 0, post);
+            yearMap[year].splice(index, 0, post);
         }
     });
 
@@ -35,10 +32,7 @@ hexo.on('generateBefore', function () {
         return b.year - a.year;
     });
 
-
-    //var json = JSON.stringify(yearPosts, null, 4);
-    //var dir = path.resolve(__dirname, '../.local_object');
-    //fs.writeFileSync(path.resolve(dir, 'yearPosts.json'), json);
+    //console.log(yearPosts);
 
     hexo.locals.set('yearPosts', yearPosts);
 });
